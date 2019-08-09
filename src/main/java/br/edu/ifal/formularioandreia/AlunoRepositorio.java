afe26f30cb572b8f277c89596573c33451b3cc8e
Andréia <andreia.leodorio@gmail.com>
2019-08-08 23:36:43 -0300

    Formulário com validação JS conectado ao banco de dados mysql

---
+++ src/main/java/br/edu/ifal/formularioandreia/AlunoRepositorio.java
@@ -0,0 +1,7 @@
+package br.edu.ifal.formularioandreia;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface AlunoRepositorio extends CrudRepository <Aluno,Long>{
+    
+}
